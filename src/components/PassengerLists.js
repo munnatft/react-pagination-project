5717b33dc57a2a39528354294d15691281d4e19f
Munna Kumar Barnwal <barnwal.munna@tftus.com>
2022-03-29 14:55:33 +0530

    css added

---
+++ src/components/PassengerLists.js
@@ -0,0 +1,93 @@
+import styles from './PassengerLists.module.css';
+
+const PassengerLists = () => {
+  return (
+    // <div className={styles.responsive}>
+        <table className={styles.table}>
+            <thead>
+                <tr>
+                    <th>S.No.</th>
+                    <th>Name</th>
+                    <th>State</th>
+                    <th>Country</th>
+                    <th>COrigin</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>ds</td>
+                    <td>re</td>
+                    <td>cvf</td>
+                    <td>43</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>ds</td>
+                    <td>re</td>
+                    <td>cvf</td>
+                    <td>43</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>ds</td>
+                    <td>re</td>
+                    <td>cvf</td>
+                    <td>43</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>ds</td>
+                    <td>re</td>
+                    <td>cvf</td>
+                    <td>43</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>ds</td>
+                    <td>re</td>
+                    <td>cvf</td>
+                    <td>43</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>ds</td>
+                    <td>re</td>
+                    <td>cvf</td>
+                    <td>43</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>ds</td>
+                    <td>re</td>
+                    <td>cvf</td>
+                    <td>43</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>ds</td>
+                    <td>re</td>
+                    <td>cvf</td>
+                    <td>43</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>ds</td>
+                    <td>re</td>
+                    <td>cvf</td>
+                    <td>43</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>ds</td>
+                    <td>re</td>
+                    <td>cvf</td>
+                    <td>43</td>
+                </tr>
+            </tbody>
+        </table>
+    // </div>
+  )
+}
+
+export default PassengerLists
